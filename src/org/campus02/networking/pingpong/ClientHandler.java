2645f664139dd35ce7d8f9f4f89512a9474f7abb
patrick-putz <Putz.Patrick.1996@gmail.com>
2022-05-14 09:48:30 +0200

    Names, High Scores

--- src/org/campus02/networking/pingpong/ClientHandler.java
+++ src/org/campus02/networking/pingpong/ClientHandler.java
@@ -2,13 +2,17 @@ package org.campus02.networking.pingpong;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ClientHandler implements Runnable {
 
     private Socket client;
+    private HashMap<String, Integer> highScore;
+    private String name;
 
-    public ClientHandler(Socket client) {
+    public ClientHandler(Socket client, HashMap<String, Integer> highScore) {
         this.client = client;
+        this.highScore = highScore;
     }
 
     public void start() {
@@ -17,16 +21,66 @@ public class ClientHandler implements Runnable {
 
             System.out.println("lese vom client -->");
             String line;
+
+            bw.write("Hallo, wie lautet Ihr Name? Antwort mit 'name:<your name>'");
+            bw.newLine();
+            bw.flush();
+
             while ((line = br.readLine()) != null) {
+
+                if(line.startsWith("name:")){
+                    String[] names = line.split(":");
+                    if(names.length == 2){
+                        name = names[1];
+
+                        synchronized (highScore){
+                            highScore.put(name, 0);
+                        }
+
+                        continue;
+                    }
+                }
+
                 switch (line) {
                     case "ping":
-                        System.out.println("Schicke PONG <---");
+                        System.out.println(name + ": Schicke PONG <---");
                         bw.write("pong");
+
+                        synchronized (highScore) {
+                            highScore.put(name, highScore.get(name) + 1);
+                        }
+
                         break;
                     case "pong":
-                        System.out.println("Schicke PING <---");
+                        System.out.println(name + ": Schicke PING <---");
                         bw.write("ping");
+
+                        synchronized (highScore) {
+                            highScore.put(name, highScore.get(name) + 2);
+                        }
+
                         break;
+                    case "myScore":
+                        System.out.println(name + " myScore");
+                        synchronized (highScore){
+                            bw.write("Your score: " + highScore.get(name));
+                        }
+                        break;
+                    case "highScore":
+                        System.out.println(name + " highScore");
+                        synchronized (highScore){
+                            for(String n : highScore.keySet()){
+                                bw.write("Score of " + n + ": " + highScore.get(n) + " points");
+                                bw.newLine();
+                            }
+                        }
+                        break;
+                    case "bye":
+                    case "exit":
+                        System.out.println(name + " leaves server");
+                        bw.write("good bye " + name);
+                        bw.flush();
+                        return;
                     default:
                         System.out.println("Schicke ERROR <---");
                         bw.write("error");
--- src/org/campus02/networking/pingpong/PingPongServer.java
+++ src/org/campus02/networking/pingpong/PingPongServer.java
@@ -3,17 +3,21 @@ package org.campus02.networking.pingpong;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class PingPongServer {
 
     public static void main(String[] args) {
+
+        HashMap<String, Integer> highScore = new HashMap<>();
+
         System.out.println("Server erzeugen");
         try (ServerSocket serverSocket = new ServerSocket(1111)) {
             while (true) {
                 System.out.println("Server wartet auf Clients...");
                 Socket client = serverSocket.accept();
                 System.out.println("Verbindung zu Client hergestellt");
-                ClientHandler clientHandler = new ClientHandler(client);
+                ClientHandler clientHandler = new ClientHandler(client, highScore);
                 // clientHandler.start();
 
                 Thread th = new Thread(clientHandler);
